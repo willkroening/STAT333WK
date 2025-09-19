4683b5ff251c3fbe5d43f04db440c64e24537e55
willkroening <willkroening003@gmail.com>
2025-09-19 13:11:18 -0500

    Update Code for Project.R

--- Code for Project.R
+++ Code for Project.R
@@ -3,4 +3,4 @@ library(tidyverse)
 data(cars)
 glipse(cars)
 my_lm <- lm(speed~distance, data=cars) #I'm building our first model
-mysummary(my_lm)
+mysummary(my_lm) #added this in github not sure if this pops up in rstudio
