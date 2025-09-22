1b6b1733f9fa71c36b3a3f1468ec91c3d7863b82
willkroening <willkroening003@gmail.com>
2025-09-22 12:13:45 -0500

    work

--- Code for Project.R
+++ Code for Project.R
@@ -6,3 +6,7 @@ glimpse(cars)
 my_lm <- lm(dist ~ speed, data = cars)  # regress stopping distance on speed
 summary(my_lm)                          # show regression results
 coef(my_lm)
+
+summary(my_lm)  
+# new comment
+
